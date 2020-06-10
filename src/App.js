2a861c61172e7830232116ea83404ac65cad3cd8
RAJ HARGON <raj.hargon@gmail.com>
2020-06-10 15:42:21 +0200

    first attempt

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Header from "./components/layout/Header";
+import Contacts from "./components/contacts/Contacts";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "./context";
+import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
+import About from "./components/pages/About";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="Contacts Mananger" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import { Consumer } from "../../context";
+import { v4 as uuidv4 } from "uuid";
+import TextInputGroup from "../layout/TextInputGroup";
+import axios from "axios";
+
+class AddContact extends Component {
+  state = {
+    name: "",
+    email: "",
+    phone: "",
+    errors: {}
+  };
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = async (dispatch, e) => {
+    e.preventDefault();
+    const { name, email, phone, errors } = this.state;
+    if (name === "") {
+      this.setState = { errors: { name: "Name is required" } };
+    }
+    if (email === "") {
+      this.setState = { errors: { email: "Email is required" } };
+    }
+    if (phone === "") {
+      this.setState = { errors: { name: "Phone is required" } };
+    }
+
+    const newContact = {
+      name,
+      email,
+      phone
+    };
+    const res = await axios.post(
+      "https://jsonplaceholder.typicode.com/users",
+      newContact
+    );
+    dispatch({ type: "ADD_CONTACT", payload: res.data });
+
+    this.setState = {
+      name: "",
+      email: "",
+      phone: "",
+      errors: {}
+    };
+    this.props.history.push("/");
+  };
+
+  render() {
+    const { name, email, phone, errors } = this.state;
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card mb-3">
+              <div className="card-header">Add Contact </div>
+              <div className="card-body ">
+                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                  <TextInputGroup
+                    label="Name"
+                    name="name"
+                    value={name}
+                    placeholder="Enter Name"
+                    onChange={this.onChange}
+                    error={errors.name}
+                  />
+                  <TextInputGroup
+                    label="Email"
+                    name="email"
+                    value={email}
+                    placeholder="Enter Email"
+                    onChange={this.onChange}
+                    type="email"
+                    error={errors.email}
+                  />
+                  <TextInputGroup
+                    label="Phone"
+                    name="phone"
+                    value={phone}
+                    placeholder="Enter Phone"
+                    onChange={this.onChange}
+                    error={errors.phone}
+                  />
+                  <input
+                    type="submit"
+                    value="Add Contact"
+                    className="btn btn-light btn-block"
+                  />
+                </form>
+              </div>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+export default AddContact;
