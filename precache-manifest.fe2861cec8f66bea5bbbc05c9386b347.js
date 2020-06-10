bf3c4ab4425942a5dd689a753e78026daf0f80d7
RAJ HARGON <raj.hargon@gmail.com>
2020-06-10 16:28:00 +0200

    Updates

---
+++ precache-manifest.fe2861cec8f66bea5bbbc05c9386b347.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe0492af2b1bff1f4a2a0491c6b5fa2",
+    "url": "/contactsmananger/index.html"
+  },
+  {
+    "revision": "fa651b3c8da1b94a14e5",
+    "url": "/contactsmananger/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "84ffdcdc30f5795a1053",
+    "url": "/contactsmananger/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "fa651b3c8da1b94a14e5",
+    "url": "/contactsmananger/static/js/2.66ac6ec1.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/contactsmananger/static/js/2.66ac6ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ffdcdc30f5795a1053",
+    "url": "/contactsmananger/static/js/main.3e1f833a.chunk.js"
+  },
+  {
+    "revision": "83b918644506081932ff",
+    "url": "/contactsmananger/static/js/runtime-main.3516409a.js"
+  }
+]);
